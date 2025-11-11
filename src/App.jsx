36b6db0e49ffc9ac9231219a55772f70475ef93f
tonyishangu <tonyishangu@gmail.com>
2025-11-11 23:05:43 +0300

    function to add a new todo

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,30 @@
 
-import React from 'react'
+import React, {useState} from 'react'
 
 function App() {
-  
+  // state to hold todos
+  const [todos, setTodos] = useState([])
+  // state to manage input
+  const [input, setInput] = useState('')
+  // state to track which todo is being editted
+  const [editId, setEditId] = useState(null)
+  // state to filter all || complete || incomplete
+  const [filter, setFilter] = useState('all')
 
+  // function to add a todo
+  const addTodo = (e) => {
+    e.preventDefault()
+    if(!input.trim()) return
+
+    const newTodo = {
+      id: Date.now(),
+      text: input.trim(),
+      completed: false
+    }
+
+    setTodos([newTodo, ...todos])
+    setInput('')
+  }
   return (
     <>
       
